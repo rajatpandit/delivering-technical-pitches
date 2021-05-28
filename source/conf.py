6a0ca798c42df43fb4f93ce545dd62f4328eb1ef
Rajat Pandit <rpandit+github@gmail.com>
2021-05-29 00:58:34 +0530

    added content

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
+html_favicon = '_static/favicon.ico'
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -86,7 +86,7 @@ ogp_site_url = "https://rajatpandit.com/delivering-technical-pitches/"
 ogp_image = "https://rajatpandit.com/delivering-technical-pitches/_static/logo.png"
 ogp_description_length = 300
 ogp_type = "article"
-ogp_use_first_image = "true"
+ogp_use_first_image = True
 
 ogp_custom_meta_tags = [
     '<meta content="@rajatpandit" property="twitter:site">',
